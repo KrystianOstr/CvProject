052a62b4a33c426ac736d0de3e4aa99ef845523c
Krystian <krystian.ostrogorski@gmail.com>
2024-03-08 07:16:32 +0100

    tailwind and cvData added

--- cvProject/src/main.tsx
+++ cvProject/src/main.tsx
@@ -1,4 +1,34 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<main></main>);
+interface Experience {
+  description: string;
+  year: number;
+}
+
+interface Cv {
+  photo: string;
+  name: string;
+  lastName: string;
+  position: string;
+  experience: Experience[];
+  education: string[];
+}
+
+const cvData: Cv = {
+  photo: `https://placehold.co/400x400`,
+  name: "Krystian",
+  lastName: "Ostr",
+  position: "Developer",
+  experience: [
+    { description: "Lorem lorem", year: 2000 },
+    { description: "Ipsum ipsum", year: 2005 },
+  ],
+  education: ["firstClass", "secondClass", "thirdClass"],
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <main className="bg-black h-screen">
+    <h1 className="text-3xl font-bold ">Hello world!</h1>
+  </main>
+);
