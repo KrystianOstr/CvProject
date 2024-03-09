77c4bb7839a3d95de5ba93107d5b643992773c42
Krystian <krystian.ostrogorski@gmail.com>
2024-03-09 17:04:48 +0100

    Styling finished

--- cvProject/src/main.tsx
+++ cvProject/src/main.tsx
@@ -16,19 +16,56 @@ interface Cv {
 }
 
 const cvData: Cv = {
-  photo: `https://placehold.co/400x400`,
+  photo: `https://placehold.co/400x400?text=KO`,
   name: "Krystian",
   lastName: "Ostr",
   position: "Developer",
   experience: [
-    { description: "Lorem lorem", year: 2000 },
-    { description: "Ipsum ipsum", year: 2005 },
+    {
+      description:
+        "Lorem loremLorem loremLorem loremLorem loremLorem loremLorem loremLorem loremLorem loremLorem lorem",
+      year: 2000,
+    },
+    {
+      description:
+        "Ipsum ipsumIpsum psum ipsumIpsum ipsumIpsum ipsumIpsum ipsumIpsum ",
+      year: 2005,
+    },
   ],
   education: ["firstClass", "secondClass", "thirdClass"],
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <main className="bg-black h-screen">
-    <h1 className="text-3xl font-bold ">Hello world!</h1>
+  <main className="mt-6  grid grid-cols-3  gap-4 text-center">
+    <header className="text-5xl font-bold text-center mb-12 col-span-3">{`CV ${cvData.name} ${cvData.lastName}`}</header>
+    <aside className="text-4xl grid justify-items-center ">
+      <h2 className="font-semibold">Personal data</h2>
+      <img
+        className="rounded-full w-60 my-10"
+        src={cvData.photo}
+        alt="myPhoto"
+      />
+      <p className="text-xl font-semibold">{`${cvData.name} ${cvData.lastName}`}</p>
+      <p className="text-xl font-medium">{cvData.position}</p>
+    </aside>
+
+    <section className="col-span-2">
+      <h2 className="text-2xl font-semibold pt-8">Experience</h2>
+      <ul className="p-4">
+        {cvData.experience.map((item) => {
+          return (
+            <li key={item.description}>
+              <strong>{item.year}</strong> - {item.description}
+            </li>
+          );
+        })}
+      </ul>
+      <h2 className="text-2xl font-semibold pt-8">Education</h2>
+      <ul className="p-4">
+        {cvData.education.map((item, i) => {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
+    </section>
   </main>
 );
